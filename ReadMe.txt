9190e1d80c99f29a6325cfe1664850c2a8552e01
Quicano <33662621+Quicano@users.noreply.github.com>
2021-09-17 17:37:33 +0200

    Finished Project

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Simply put your PDF's into the Data Directory and run the Main class
+Project needs to be installed first (mvn install)
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
+    public static List<String> readPDFFiles(){
+        File DataDir = new File("Data");
+        ArrayList<File> fileList = new ArrayList<>(Arrays.asList(DataDir.listFiles()));
+        ArrayList<String> pdfList = new ArrayList<>();
+        for (File f : fileList){
+            if(f.getName().endsWith(".pdf")){
+                pdfList.add(f.getName());
+            }
+        }
+        return pdfList;
+    }
 
     public static void main(String[] args) throws Exception {
         PDFToXML converter = new PDFToXML();
-        String xmlFile = converter.convertToXml("Data/wpss87-framsida-pdf.pdf");
+        List<String> Filenames = readPDFFiles();
+        for (String s : Filenames){
+            converter.convertToXml("Data/"+s);
+        }
     }
 }
